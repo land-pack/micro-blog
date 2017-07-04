bcad535c60540d4cb4387f8176ffb3537416ef87
landpack <ada.frank.ak@gmail.com>
2017-07-04 14:15:08 +0800

    [add] first part

---
+++ micro-blog/app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+"""
+If you are wondering why the import statement is at the end and not at 
+the beginning of the script as it is always done, the reason is to avoid 
+circular references, because you are going to see that the views module 
+needs to import the app variable defined in this script. Putting the 
+import at the end avoids the circular import error.
+"""
+from app import views
