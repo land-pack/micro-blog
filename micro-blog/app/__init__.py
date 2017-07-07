d72a9e8c454f2d881560aef63f538aea65716718
landpack <ada.frank.ak@gmail.com>
2017-07-07 20:34:17 +0800

    [add] database with ORM

--- micro-blog/app/__init__.py
+++ micro-blog/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("config")
+db = SQLAlchemy(app)
 
 """
 If you are wondering why the import statement is at the end and not at 
@@ -10,4 +12,4 @@ circular references, because you are going to see that the views module
 needs to import the app variable defined in this script. Putting the 
 import at the end avoids the circular import error.
 """
-from app import views
+from app import views, models
